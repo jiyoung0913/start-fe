d6d6e47a7d0f718fbeaf17cd1955687656b02d3d
jiyoung0913 <rhwldud0913@naver.com>
2021-04-15 20:07:28 +0900

    quiz6

---
+++ 06/quiz6.js
@@ -0,0 +1,22 @@
+const $num = document.getElementById('num');
+const $btn = document.getElementById('btn');
+const $result = document.getElementById('rasult');
+
+function print(){
+    const result = [];
+    const num = $num.Value;
+
+    if(isNaN(num)){
+        alert('숫자를 입력하세요');
+        return;
+    }
+
+    for(let i = 1; i <= 9; i++){
+        result.push(`${num} x ${i} = ${num * i}`);
+    }
+    $result.innerHTML = result.join('<br>'); 
+
+    
+}
+
+$btn.addEventListener('click',print)
