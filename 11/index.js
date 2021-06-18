618525737cc6b8e6909e8843ddbcc96e54015fe5
jiyoung0913 <rhwldud0913@naver.com>
2021-06-18 13:27:07 +0900

    퀴즈

---
+++ 11/index.js
@@ -0,0 +1,22 @@
+import { render } from './html-render';
+import inputForm from './input-form';
+
+import './todos.css';
+
+const todos = [];
+const $result = document.querySelector('#result');
+
+$result.addEventListener('click', (event) => {
+  if (event.target.className === 'delete') {
+    const { index } = event.target.parentElement.dataset;
+    todos.splice(index, 1);
+    render(todos);
+  } else if (event.target.className === 'toggle-checked') {
+    const { index } = event.target.parentElement.dataset;
+    todos[index].isDone = !todos[index].isDone;
+    render(todos);
+  }
+});
+
+inputForm.init(todos);
+render(todos);
